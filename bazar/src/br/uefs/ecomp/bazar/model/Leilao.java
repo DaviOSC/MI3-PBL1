520a5077c45e5789447bfc36ae12b435efb6d7e2
Davi Oliveira <davioliveira541@outlook.com>
2024-04-05 16:50:10 -0300

    Criação da classe Leilão e definição do construtor

--- bazar/src/br/uefs/ecomp/bazar/model/Leilao.java
+++ bazar/src/br/uefs/ecomp/bazar/model/Leilao.java
@@ -1,5 +1,26 @@
 package br.uefs.ecomp.bazar.model;
+import java.util.List;
+import java.util.Iterator;
+
 public class Leilao
 {
     private double precoMinimo;
+    private double incrementoMinimo;
+    private int status;
+    private Produto produto;
+    private Usuario vendedor;
+    
+    List<Lance> lances;
+    List<Usuario> participantes;
+    
+    public Leilao(double lPrecoMinimo, double lIncrementoMinimo, Usuario lVendedor, Produto lProduto)
+    {
+        this.precoMinimo = lPrecoMinimo;
+        this.incrementoMinimo = lIncrementoMinimo;
+        this.vendedor = lVendedor;
+        this.produto = lProduto;
+    }
+    
+    
+    
 }
